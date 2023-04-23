a679a74ab39154c6ef553df6cf08b1862e6baa53
LuzSallietti <luzsallietti@gmail.com>
2023-04-22 22:40:27 -0300

    fetching points succesfully on Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from "react";
+import { getData } from "../storage/firebaseMethods";
 import AnchorBtn from "../components/AnchorBtn";
 
 const Home = () => {
+  const [points, setPoints] = useState(null);
+
+  const fetchData = async () => {
+    const data = await getData("points");
+    setPoints(data)
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className=" flex flex-col items-center justify-center">
       <h1>Punto celíaco</h1>
       <h2>Home</h2>
       <br></br>
+      {points && points.map(point => {
+        return(
+            <div key={point.id}>
+                <h2>{point.data.name}</h2>
+                <p>{point.data.address}</p>
+
+            </div>
+        )
+      })}
       <AnchorBtn path="/puntos/crear" text="Crear un punto" />
     </div>
   );
